c9c69b67b50ddacf70263e82f61fa577cd4d9f44
wenkhai2005@gmail.com <wenkhai2005@gmail.com>
2025-02-20 15:09:35 +0800

    item dismissible and firebase

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,22 +1,89 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
-
+  
   @override
   State<GroceryList> createState() => _GroceryListState();
 }
 
 class _GroceryListState extends State<GroceryList> {
+  Widget content = Center(
+    child: Text(
+      'No item added',
+      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+  );
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
 
-  void _addItem() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => NewItem(),),);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+      'flutter-prep-be8b3-default-rtdb.asia-southeast1.firebasedatabase.app',
+      'shopping-list.json',
+    );
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = jsonDecode(response.body);
+    final List<GroceryItem> _loadedItems = [];
+    for (final item in listData.entries) {
+      final category =
+          categories.entries
+              .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'],
+              )
+              .value;
+      _loadedItems.add(
+        GroceryItem(
+          id: item.key,
+          name: item.value['name'],
+          quantity: item.value['quantity'],
+          category: category,
+        ),
+      );
+    }
+    setState(() {
+      _groceryItems = _loadedItems;
+      _isLoading = false;
+    });
+  }
+
+  void _addItem() async {
+    final newItem = await Navigator.of(
+      context,
+    ).push<GroceryItem>(MaterialPageRoute(builder: (ctx) => NewItem()));
+
+    if (newItem == null){
+      return;
+    }
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      content = const Center(child: CircularProgressIndicator(),);
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -27,36 +94,58 @@ class _GroceryListState extends State<GroceryList> {
         ),
         actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
       ),
-      body: ListView(
-        children: [
-          for (final item in groceryItems)
-            InkWell(
-              onTap: () {},
-              child: Container(
-                padding: EdgeInsets.all(16),
-                child: Row(
-                  children: [
-                    Icon(Icons.square, color: item.category.color),
-                    SizedBox(width: 16),
-                    Text(
-                      item.name,
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.secondary,
+      body:
+          _groceryItems.isEmpty
+              ? content
+              : ListView(
+                children: [
+                  for (final item in _groceryItems)
+                    Dismissible(
+                      key: ValueKey(item),
+                      onDismissed: (direction) => _removeItem(item),
+                      background: Container(
+                        color: Colors.red, // Background color while swiping
+                        alignment: Alignment.centerRight,
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Icon(Icons.delete, color: Colors.white),
+                      ),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 20,
+                            horizontal: 16,
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(Icons.square, color: item.category.color),
+                              SizedBox(width: 16),
+                              Text(
+                                item.name,
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.titleMedium!.copyWith(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                item.quantity.toString(),
+                                style: Theme.of(
+                                  context,
+                                ).textTheme.titleMedium!.copyWith(
+                                  color:
+                                      Theme.of(context).colorScheme.secondary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                    Spacer(),
-                    Text(
-                      item.quantity.toString(),
-                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                        color: Theme.of(context).colorScheme.secondary,
-                      ),
-                    ),
-                  ],
-                ),
+                ],
               ),
-            ),
-        ],
-      ),
     );
   }
 }
